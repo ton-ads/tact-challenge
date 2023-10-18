b9172bc621ea351026ab4caa936aee0c998b894d
ton-ads <no-reply@t7y.ru>
2023-10-18 23:05:54 +0300

    5 test-1

--- tests/Task5.spec.ts
+++ tests/Task5.spec.ts
@@ -1,16 +1,38 @@
-import { Blockchain, SandboxContract } from '@ton/sandbox';
-import '@ton/test-utils';
-import { toNano } from 'ton-core';
+import { Blockchain, SandboxContract, printTransactionFees, TreasuryContract } from '@ton-community/sandbox';
+import '@ton-community/test-utils';
+import { toNano, Address, beginCell } from 'ton-core';
 import { Task5 } from '../wrappers/Task5';
+import { flattenTransaction } from "@ton-community/test-utils";
 
 describe('Task5', () => {
     let blockchain: Blockchain;
     let task5: SandboxContract<Task5>;
+    let deployer: SandboxContract<TreasuryContract>;
+    let nft: SandboxContract<TreasuryContract>;
+    let sender: SandboxContract<TreasuryContract>;
 
-    beforeEach(async () => {
+    it('should deploy', async () => {
         blockchain = await Blockchain.create();
-        task5 = blockchain.openContract(await Task5.fromInit());
-        const deployer = await blockchain.treasury('deployer');
+
+        /*
+        // https://github.com/ton-org/sandbox#viewing-logs
+        blockchain.verbosity = {
+            print: true,
+            blockchainLogs: true,
+            vmLogs: "none",
+            debugLogs: true,
+        }
+        */
+
+        deployer = await blockchain.treasury('deployer');
+        nft = await blockchain.treasury('nft');
+        sender = await blockchain.treasury('sender');
+
+        task5 = blockchain.openContract(await Task5.fromInit(
+            1n,
+            deployer.address as Address
+        ));
+
         const deployResult = await task5.send(
             deployer.getSender(),
             {
@@ -27,9 +49,58 @@ describe('Task5', () => {
             deploy: true,
             success: true,
         });
+
+        console.log('contract address', task5.address);
+        console.log('deployer (admin) address', deployer.address);
+        console.log('nft address', nft.address);
+        console.log('sender address', sender.address);
     });
 
-    it('test', async () => {
+    it('should reject AdminWithdrawalAllNFTs: Invalid sender ', async () => {
+        const shouldResult = await task5.send(
+            sender.getSender(),
+            {
+                value: toNano('0.01'),
+            },
+            {
+                $$type: 'AdminWithdrawalAllNFTs',
+                queryId: 0n
+            }
+        );
+
+        printTransactionFees(shouldResult.transactions);
+
+        expect(shouldResult.transactions).toHaveTransaction({
+            from: sender.address,
+            to: task5.address,
+            success: false,
+        });
+    });
+
+    it('should reject AdminWithdrawalAllNFTs: Insufficent funds ', async () => {
+        const sentMessageResult = await task5.send(
+            deployer.getSender(),
+            {
+                value: toNano('0.02'),
+            },
+            {
+                $$type: 'AdminWithdrawalAllNFTs',
+                queryId: 0n
+            }
+        );
+
+        /*
+        const arr = sentMessageResult.transactions.map(tx => flattenTransaction(tx));
+        console.log(arr);
+        */
+
+        printTransactionFees(sentMessageResult.transactions);
+
+        expect(sentMessageResult.transactions).toHaveTransaction({
+            from: deployer.address,
+            to: task5.address,
+            success: false,
+        });
     });
 });
 
--- wrappers/Task5.compile.ts
+++ wrappers/Task5.compile.ts
@@ -3,4 +3,7 @@ import { CompilerConfig } from '@ton/blueprint';
 export const compile: CompilerConfig = {
     lang: 'tact',
     target: 'contracts/5.tact',
+    options: {
+        debug: true,
+    },
 };
